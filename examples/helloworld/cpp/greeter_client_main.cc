56570ff30a3487350a33009bd5136085a0c8b686
Paul Johnston <pcj@pubref.org>
2016-08-12 10:09:38 -0600

    missing cpp main client method

--- examples/helloworld/cpp/greeter_client_main.cc
+++ examples/helloworld/cpp/greeter_client_main.cc
@@ -35,25 +35,18 @@
 
 #include "greeter_client.h"
 
-std::string GreeterClient::SayHello(const std::string& user) {
-  // Data we are sending to the server.
-  HelloRequest request;
-  request.set_name(user);
+int main(int argc, char** argv) {
 
-  // Container for the data we expect from the server.
-  HelloReply reply;
+  // Instantiate the client. It requires a channel, out of which the
+  // actual RPCs are created. This channel models a connection to an
+  // endpoint (in this case, localhost at port 50051). We indicate
+  // that the channel isn't authenticated (use of
+  // InsecureCredentials()).
+  GreeterClient greeter(
+      grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+  std::string user("world");
+  std::string reply = greeter.SayHello(user);
+  std::cout << "Greeter received: " << reply << std::endl;
 
-  // Context for the client. It could be used to convey extra information to
-  // the server and/or tweak certain RPC behaviors.
-  ClientContext context;
-
-  // The actual RPC.
-  Status status = stub_->SayHello(&context, request, &reply);
-
-  // Act upon its status.
-  if (status.ok()) {
-    return reply.message();
-  } else {
-    return "RPC failed";
-  }
+  return 0;
 }
